ff6211c6bebf555cfc73f2d857fe42f6b6f9d574
mehanik87 <mehanik_8_7@i.ua>
2016-02-28 21:14:41 +0200

    Update ConnectionProviderTest.java

--- src/test/java/ConnectionProviderTest.java
+++ src/test/java/ConnectionProviderTest.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 /**
- * Created by Àäìèíèñòðàòîð on 26.02.2016.
+ * Created by on 26.02.2016.
  */
 @RunWith(JUnit4.class)
 public class ConnectionProviderTest extends TestCase {
